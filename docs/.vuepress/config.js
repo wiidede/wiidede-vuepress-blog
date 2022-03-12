2e8a91b455b2e0caf543e3b76f3b3b8bace7dc6e
wiidede <wiixdede@gmail.com>
2022-03-12 18:56:26 +0800

    problem: how to delete online theme with correct package nest

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,12 @@
 const head = require('./config/head.js');
 const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
+const path = require("path");
 
 module.exports = {
   // theme: 'vdoing', // 使用npm包主题
-  theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
+  theme: path.resolve(__dirname, '../../theme-vdoing/index.js'), // 使用本地主题
 
   title: "Wiidede's blog",
   description: 'wiidede的个人博客，主要写写前端',
